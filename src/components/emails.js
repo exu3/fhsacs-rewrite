d0c0178786375249a5e8d4f762a749f82e463b7b
eilla1 <exu6056@gmail.com>
2021-03-02 21:36:50 -0800

    :sparkles: email list card

--- src/components/emails.js
+++ src/components/emails.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { Box, Text, Button, Input, FormControl, FormLabel, FormHelperText, ChakraProvider } from '@chakra-ui/react'
+import { Box, Text, Button, Input, FormControl, FormLabel, FormHelperText, ChakraProvider, InputGroup, InputRightElement } from '@chakra-ui/react'
 
 const Emails = () => (
   <ChakraProvider>
-    <Box bg="gray.100" textAlign="center" p="5" w="60%" mx="auto" borderRadius="15">
-        <FormControl id="email">
-        <FormLabel>Join our mailing list!</FormLabel>
-        <FormHelperText>
-            Enter your email below to stay updated on the latest news, upcoming events, and club meetings.
-        </FormHelperText>
-        </FormControl>
-        <Input type="email" placeholder="Email" focusBorderColor="blue.200" isRequired="true" />
+    <Box bg="gray.100" textAlign="center" p="5" w="60%" mx="auto" borderRadius="lg" p={4}>
+        <form>
+            <FormControl id="email">
+                <FormLabel>Join our mailing list!</FormLabel>
+                <FormHelperText textAlign="left">
+                    Enter your email below to stay updated on the latest news, upcoming events, and club meetings.
+                </FormHelperText><br />
+            </FormControl>
+            <InputGroup>
+                <Input type="email" placeholder="Email" focusBorderColor="blue.200" isRequired="true" />
+                <InputRightElement children={<Button width={20} type="submit" bg="green.200" _hover={{ bg: "green.300" }}>✓</Button>} />
+            </InputGroup>
+        </form>
+        {/* <Button type="submit" _hover={{ bg: "green.300" }}>Submit</Button> */}
     </Box>
   </ChakraProvider>
 )
