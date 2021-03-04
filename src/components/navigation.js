5de09d5d1c2e565b0c755f95b141874721eee216
eilla1 <exu6056@gmail.com>
2021-03-03 23:35:31 -0800

    :ambulance: new navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import styles from './navigation.module.css'
+import { ChakraProvider, HStack, Box, Center } from '@chakra-ui/react'
+// import styles from './navigation.module.css'
 
-export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/about/">About</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/updates/">Latest</Link>
-      </li>
-    </ul>
-  </nav>
+const Navigation = () => (
+  <ChakraProvider>
+    <Box bg="gray.100" p="5" mx="auto" textAlign="center">
+        <Center>
+          <HStack spacing="25px">
+            <Link to="/">Home</Link>
+            <Link to="/events">Upcoming Events</Link>
+            <Link to="/updates">Meetings</Link>
+            <a href="mailto:foothillacs@gmail.com">Contact</a>
+          </HStack>
+        </Center>
+    </Box>
+  </ChakraProvider>
 )
+
+export default Navigation
