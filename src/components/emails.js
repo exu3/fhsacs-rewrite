6674a39169cd0f1e55915da155da9989f3468987
eilla1 <exu6056@gmail.com>
2021-03-06 13:21:22 -0800

    add form value

--- src/components/emails.js
+++ src/components/emails.js
@@ -4,7 +4,7 @@ import { Box, Button, Input, FormControl, FormLabel, FormHelperText, ChakraProvi
 const Emails = () => (
   <ChakraProvider>
     <Box bg="white" textAlign="center" p="5" w="60%" mx="auto" borderRadius="lg" borderColor="gray.200" borderWidth={1} boxShadow="md">
-        <form>
+        <form action="https://docs.google.com/forms/u/0/d/e/1FAIpQLScCZg4T4q4sLK1TygnIIjGcRq2z_rNGrzplnwQcDa3G-FEPNw/formResponse" method="POST">
             <FormControl id="email">
                 <FormLabel>Join our mailing list!</FormLabel>
                 <FormHelperText textAlign="left">
@@ -13,8 +13,8 @@ const Emails = () => (
             </FormControl>
             <InputGroup>
             <HStack w="100%">
-                <Input type="email" placeholder="name@example.com" focusBorderColor="blue.200" isRequired="true" w="100%" />
-                <Button width={20} type="submit" bg="green.200" _hover={{ bg: "green.300" }} px={8}> Sign Up </Button>
+                <Input type="email" placeholder="name@example.com" focusBorderColor="blue.200" isRequired="true" w="100%" name="entry.1553078721" />
+                <Button width={20} type="submit" method="POST" target="_blank" bg="green.200" _hover={{ bg: "green.300" }} px={8}> Sign Up </Button>
             </HStack>
             </InputGroup>
         </form>
